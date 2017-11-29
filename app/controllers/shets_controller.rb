e85c62dfd7a5ce8d44d1aa38d7dab0780a8a3a92
arnaud de valbray <avalbray@gmail.com>
2017-11-29 13:48:38 +0100

    show

--- app/controllers/shets_controller.rb
+++ app/controllers/shets_controller.rb
@@ -2,4 +2,29 @@ class ShetsController < ApplicationController
   def index
     @shetlands = Shetland.all
   end
+
+  def show
+    @shetland = Shetland.find(params[:id])
+  end
+
+  # def new
+  #   @cocktail = Cocktail.new
+  # end
+
+  # def create
+  #   @cocktail = Cocktail.new(cocktail_params)
+
+  #   if @cocktail.save
+  #     redirect_to cocktail_path(@cocktail)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  # def shetland_params
+  #   params.require(:shetland).permit(:name)
+  # end
+
 end
