c00b6293139140837093984a485b2765f9ddcfa9
arnaud de valbray <avalbray@gmail.com>
2017-11-30 17:10:19 +0100

    Add shet creation with pic

--- app/controllers/my_shets_controller.rb
+++ app/controllers/my_shets_controller.rb
@@ -1,14 +1,27 @@
 class MyShetsController < ApplicationController
-  # def index
-  #   @shetlands = Shetland.all
-  # end
+  def index
+    @shetlands = Shetland.all
+  end
 
   # def details
   # end
 
   def create
+     @shetland = Shetland.new(shetland_params)
+     @shetland.user = current_user
+
+    if @shetland.save
+      redirect_to my_shets_path(@shetland)
+    else
+      render "myshets/new"
+    end
   end
 
   def new
+    @shetland = Shetland.new
+  end
+
+  def shetland_params
+    params.require(:shetland).permit(:name, :price_per_hour, :photo, :photo_cache)
   end
 end
